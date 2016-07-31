d028da555fffc17f99eaa060a4b65152ced500d7
sjj <henusjj@foxmail.com>
2016-07-31 12:16:39 +0800

    ok

---
+++ 3DLable/3DLable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DLable
+//
+//  Created by 史玉金 on 16/7/31.
+//  Copyright © 2016年 史玉金. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3DLable/3DLable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLSphereView
+//
+//  Created by 史晶晶 on 16/4/4.
+//  Copyright © 2016年 xiaolong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
